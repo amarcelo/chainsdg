461ada07b8baf3a93f24fcf3f8e19fe894393a8d
Tony <antonmarcelo@gmail.com>
2023-04-28 21:14:45 -0300

    Update mytoken.sol

--- contracts/mytoken.sol
+++ contracts/mytoken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /// @custom:security-contact antonmarcelo@gmail.com
+///Brazuca is the token name of the company with the certification
 contract Brazuca is ERC20 {
     constructor() ERC20("Brazuca", "BZC") {
         _mint(msg.sender, 2000 * 10 ** decimals());
